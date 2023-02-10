59f41cfd00a80d3535c04efbcc70768764ea1c4c
metrysaa20 <antonius1999@yahoo.de>
2023-02-10 14:11:07 +0100

    changed server port

--- app/util/config.js
+++ app/util/config.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 // set port with Environment variable "SERVER_PORT" or use 8080 as default.
-const PORT = process.env.SERVER_PORT || 8081;
+const PORT = process.env.SERVER_PORT || 8089;
 // set runtime environment to switch between logic, depending on environment (production, development, ...). See ../notes/model.js
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
